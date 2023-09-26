f62f569428d6194841300b816c071d0e8ab82bff
Stráhl Balázs <balazs.strahl@gmail.com>
2023-09-26 14:37:58 +0200

    projectContainsTheFile

--- src/projects.ts
+++ src/projects.ts
@@ -33,6 +33,22 @@ export class MyProjects {
 		return this.jsonData.projects.some(project => project.name === projectName);
 	}
 
+	projectContainsTheFile(projectName: string, absPath: string, relPath: string): boolean {
+		const proj: Project | undefined = this.jsonData.projects.find(project => project.name === projectName);
+
+		if(!proj)
+			return true;
+
+		if(proj.files.some(file => file.relativePath !== relPath))
+			return false;
+
+		//TODO: Other file with each relativ path. Swap?
+		// if(proj.files.some(file => file.absolutPath === absPath))
+		// 	return true;
+
+		return true;
+	}
+
 	createNewProject(projectName: string): void {
 		if(this.projectExists(projectName)) 
 			vscode.window.showInformationMessage(`A project called ${projectName} already exists!`);
@@ -49,6 +65,10 @@ export class MyProjects {
 			const updatedJsonString: string = JSON.stringify(this.jsonData, null, 2);
 			fs.writeFileSync(this.jsonPath, updatedJsonString, 'utf-8');
 		}
+	}
+
+	addFileToProject(projectName: string, absPath: string, relPath: string): void {
+
 	}
 }
 
