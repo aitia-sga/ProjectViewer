0a7eb8947773e31a0594698594bdc6194bc7a3e1
Stráhl Balázs <balazs.strahl@gmail.com>
2023-09-27 18:15:41 +0200

    Code cleaning

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,4 @@
 
-/*
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as projects from './projects';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	const myProjects = new projects.MyProjects('/home/strabi/Munkak/AITIA/StrahlExlorer/projectviewer/projects.json');
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.projectviewer.addToProject', (fileUri: vscode.Uri) => {
-			if (fileUri) {
-				vscode.window.showInformationMessage(`Adding ${path.basename(fileUri.fsPath)} to project.`);
-				myProjects.addFileToProject("Proj3", "P3D1", fileUri.fsPath, path.basename(fileUri.fsPath));
-			}
-		}),
-		
-		vscode.commands.registerCommand('extension.projectviewer.newProject', async () => {
-			vscode.window.showInformationMessage('A gombot megnyomták!');
-			
-			const userInput = await vscode.window.showInputBox({
-				prompt: 'Add your input hereddd',
-				placeHolder: 'Placeholder text'
-			});
-			
-			if (userInput) {
-				vscode.window.showInformationMessage(`You entered: ${userInput}`);
-				myProjects.createNewProject(userInput);
-			} else {
-				vscode.window.showInformationMessage('No input provided');
-			}
-	
-		})
-	
-	);
-}
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
-*/
-
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
