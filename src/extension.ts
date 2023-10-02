f540d808839a1e51a5c563e1181d29fd04c99987
Stráhl Balázs <balazs.strahl@gmail.com>
2023-10-02 09:11:09 +0200

    replace space to tabs

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,22 @@ import * as projects from './projects';
 
 export function activate(context: vscode.ExtensionContext) {
 	const myProjects = new projects.MyProjects(path.join(context.extensionPath, 'projects.json'));
-    const activeProjectsPath = path.join(vscode.workspace.workspaceFolders![0].uri.fsPath, '.vscode', 'activeProjects.json');
+	const activeProjectsPath = path.join(vscode.workspace.workspaceFolders![0].uri.fsPath, '.vscode', 'activeProjects.json');
 
-    let activeProjectsData: { activeProjects: string[] };
-    try {
-        activeProjectsData = JSON.parse(fs.readFileSync(activeProjectsPath, 'utf8'));
-    } catch {
-        activeProjectsData = { activeProjects: [] };
-    }
+	let activeProjectsData: { activeProjects: string[] };
+	try {
+		activeProjectsData = JSON.parse(fs.readFileSync(activeProjectsPath, 'utf8'));
+	} catch {
+		activeProjectsData = { activeProjects: [] };
+	}
 
-    const projectsProvider = new ProjectsTreeProvider(myProjects.getProjects());
-    vscode.window.registerTreeDataProvider('projectsView', projectsProvider);
+	const projectsProvider = new ProjectsTreeProvider(myProjects.getProjects());
+	vscode.window.registerTreeDataProvider('projectsView', projectsProvider);
 
-    const activeProjectsProvider = new ActiveProjectsTreeProvider(myProjects.getProjects(), activeProjectsData.activeProjects);
-    vscode.window.registerTreeDataProvider('activeProjectsView', activeProjectsProvider);
+	const activeProjectsProvider = new ActiveProjectsTreeProvider(myProjects.getProjects(), activeProjectsData.activeProjects);
+	vscode.window.registerTreeDataProvider('activeProjectsView', activeProjectsProvider);
 
-    context.subscriptions.push(
+	context.subscriptions.push(
 		vscode.commands.registerCommand('projectViewer.addProjectToActive', (project) => {
 			if (!activeProjectsData.activeProjects.includes(project.name)) {
 				activeProjectsData.activeProjects.push(project.name);
@@ -42,20 +42,20 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		}),
 
-        vscode.commands.registerCommand('projectViewer.createNewFolder', async (project) => {
-            if(!myProjects.containsProject) {vscode.window.showErrorMessage('The selected project cannot be found!'); return; }
-            
-            const userInput = await vscode.window.showInputBox({
+		vscode.commands.registerCommand('projectViewer.createNewFolder', async (project) => {
+			if(!myProjects.containsProject) {vscode.window.showErrorMessage('The selected project cannot be found!'); return; }
+			
+			const userInput = await vscode.window.showInputBox({
 				prompt: 'Enter the name of the folder!',
 				placeHolder: 'Folder name'
 			});
 
-            if (userInput) {
+			if (userInput) {
 				myProjects.createNewFolder(project, userInput);
 				activeProjectsProvider.refresh()
 			} else
 				vscode.window.showInformationMessage('No input provided');	
-        }),
+		}),
 
 		vscode.commands.registerCommand('projectViewer.newProject', async () => {
 			const userInput = await vscode.window.showInputBox({
@@ -70,118 +70,118 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.window.showInformationMessage('No input provided');	
 		}),
 
-        vscode.commands.registerCommand('projectViewer.createNewFolder', async (project) => {
-        }),
+		vscode.commands.registerCommand('projectViewer.createNewFolder', async (project) => {
+		}),
 
 		vscode.commands.registerCommand('projectViewer.addToProject', async (fileUri: vscode.Uri) => {
 
-            let selectedProject;
+			let selectedProject;
 
 			if (!fileUri)  { vscode.window.showErrorMessage('fileUri is empty!'); return; }
-            
-            const projectNames = myProjects.getProjects().map(project => project.name);
-            const selectedProjectName = await vscode.window.showQuickPick(projectNames, {placeHolder: 'Please select the project!'});
-            
-            if (selectedProjectName) {
-                selectedProject = myProjects.getProjects().find(project => project.name === selectedProjectName);
-                if (!selectedProject) { vscode.window.showErrorMessage('The selected project cannot be found!'); return; }
-                
-                
-            } else return;
-            
-            const directoryNames = selectedProject.directorys.map(directory => directory.name);
-            const selectedDirectoryName = await vscode.window.showQuickPick(directoryNames, {placeHolder: 'Please select the directory!'});
-            
-            if (!selectedDirectoryName) return;
+			
+			const projectNames = myProjects.getProjects().map(project => project.name);
+			const selectedProjectName = await vscode.window.showQuickPick(projectNames, {placeHolder: 'Please select the project!'});
+			
+			if (selectedProjectName) {
+				selectedProject = myProjects.getProjects().find(project => project.name === selectedProjectName);
+				if (!selectedProject) { vscode.window.showErrorMessage('The selected project cannot be found!'); return; }
+				
+				
+			} else return;
+			
+			const directoryNames = selectedProject.directorys.map(directory => directory.name);
+			const selectedDirectoryName = await vscode.window.showQuickPick(directoryNames, {placeHolder: 'Please select the directory!'});
+			
+			if (!selectedDirectoryName) return;
 
-            myProjects.addFileToProject(selectedProjectName, selectedDirectoryName, fileUri.fsPath, path.basename(fileUri.fsPath));
-            activeProjectsProvider.refresh();
+			myProjects.addFileToProject(selectedProjectName, selectedDirectoryName, fileUri.fsPath, path.basename(fileUri.fsPath));
+			activeProjectsProvider.refresh();
 		}),
 
-        vscode.commands.registerCommand('projectViewer.removeFromProject', async (removedFile: projects.File) => {
-            myProjects.removeFileFromProject(removedFile);
-            activeProjectsProvider.refresh();
-        })
+		vscode.commands.registerCommand('projectViewer.removeFromProject', async (removedFile: projects.File) => {
+			myProjects.removeFileFromProject(removedFile);
+			activeProjectsProvider.refresh();
+		})
 	);
 }
 
 class ProjectsTreeProvider implements vscode.TreeDataProvider<any> {
 	private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
-    readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+	readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
 
-    constructor(private projects: any[]) {}
+	constructor(private projects: any[]) {}
 
 	refresh(): void {
-        this._onDidChangeTreeData.fire(undefined);
-    }
+		this._onDidChangeTreeData.fire(undefined);
+	}
 
-    getTreeItem(element: any): vscode.TreeItem {
-        return {
-            label: element.name,
-            contextValue: 'project',
-            collapsibleState: vscode.TreeItemCollapsibleState.None,
-        };
-    }
+	getTreeItem(element: any): vscode.TreeItem {
+		return {
+			label: element.name,
+			contextValue: 'project',
+			collapsibleState: vscode.TreeItemCollapsibleState.None,
+		};
+	}
 
-    getChildren(element?: any): Thenable<any[]> {
-        if (!element) {
-            return Promise.resolve(this.projects);
-        }
-        return Promise.resolve([]);
-    }
+	getChildren(element?: any): Thenable<any[]> {
+		if (!element) {
+			return Promise.resolve(this.projects);
+		}
+		return Promise.resolve([]);
+	}
 }
 
 class ActiveProjectsTreeProvider implements vscode.TreeDataProvider<any> {
-    private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
-    readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+	private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+	readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
 
-    constructor(private projectsData: any, private activeProjectsNames: string[]) {}
+	constructor(private projectsData: any, private activeProjectsNames: string[]) {}
 
-    refresh(): void {
-        this._onDidChangeTreeData.fire(undefined);
-    }
+	refresh(): void {
+		this._onDidChangeTreeData.fire(undefined);
+	}
 
-    getTreeItem(element: any): vscode.TreeItem {
-        if ('directorys' in element) {
-            return {
-                label: element.name,
+	getTreeItem(element: any): vscode.TreeItem {
+		if ('directorys' in element) {
+			return {
+				label: element.name,
 				contextValue: 'activeProject',
-                collapsibleState: vscode.TreeItemCollapsibleState.Collapsed
-            };
-        } else if ('files' in element) {
-            return {
-                label: element.name,
-                contextValue: 'directory',
-                collapsibleState: vscode.TreeItemCollapsibleState.Collapsed
-            };
-        } else if ('name' in element && !('directorys' in element)) {
-            return {
-                label: element.name,
-                collapsibleState: vscode.TreeItemCollapsibleState.None
-            };
-        } else {
-            return {
-                label: element.fileName,
-                collapsibleState: vscode.TreeItemCollapsibleState.None,
-                contextValue: 'file',
-                command: {
-                    command: 'vscode.open',
-                    arguments: [vscode.Uri.file(element.absolutPath)],
-                    title: 'Open File'
-                }
-            };
-        }
-    }
+				collapsibleState: vscode.TreeItemCollapsibleState.Collapsed
+			};
+		} else if ('files' in element) {
+			return {
+				label: element.name,
+				contextValue: 'directory',
+				collapsibleState: vscode.TreeItemCollapsibleState.Collapsed
+			};
+		} else if ('name' in element && !('directorys' in element)) {
+			return {
+				label: element.name,
+				collapsibleState: vscode.TreeItemCollapsibleState.None
+			};
+		} else {
+			return {
+				label: element.fileName,
+				collapsibleState: vscode.TreeItemCollapsibleState.None,
+				contextValue: 'file',
+				command: {
+					command: 'vscode.open',
+					arguments: [vscode.Uri.file(element.absolutPath)],
+					title: 'Open File'
+				}
+			};
+		}
+	}
 
-    getChildren(element?: any): Thenable<any[]> {
-        if (!element) {
-            return Promise.resolve(this.projectsData.filter((project: any) => this.activeProjectsNames.includes(project.name)));
-        } else if ('directorys' in element) {
-            return Promise.resolve(element.directorys);
-        } else if ('files' in element) {
-            return Promise.resolve(element.files);
-        } else {
-            return Promise.resolve([]);
-        }
-    }
+	getChildren(element?: any): Thenable<any[]> {
+		if (!element) {
+			return Promise.resolve(this.projectsData.filter((project: any) => this.activeProjectsNames.includes(project.name)));
+		} else if ('directorys' in element) {
+			return Promise.resolve(element.directorys);
+		} else if ('files' in element) {
+			return Promise.resolve(element.files);
+		} else {
+			return Promise.resolve([]);
+		}
+	}
 }
